53ebf9f9932c8415612da6d8c6566dfff784c4c8
DenzelSullivan <denzel.w.sullivan@gmail.com>
2020-01-22 20:52:29 -0500

    Restructured app into more components

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+    // creating a list of class names and dynamically changing them
+    const assignedClasses = [];
+
+    let btnClass = '';
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if (props.persons.length <= 2) {
+        assignedClasses.push(classes.red);
+    }
+    if (props.persons.length <= 1) {
+        assignedClasses.push(classes.bold);
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1> Hi, im a react app</h1>
+            <p className={assignedClasses.join(' ')}> This is really working</p>
+            <button className={btnClass}
+                onClick={props.clicked}>
+                Toggle Persons
+            </button>
+        </div>
+    );
+};
+
+export default cockpit;
